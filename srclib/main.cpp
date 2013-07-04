858c220ad3f66c7200da1f3f1631b29ed68bb5a7
lacaulac <d0lph1n@me.com>
2013-07-04 19:58:01 +0200

    First Commit
    The first commit with a base code

---
+++ srclib/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+string version = "Pre-Alpha";
+int main()
+{
+	cout << "Bienvenue dans la version " << version << " de ConsoleRPG!" << endl;
+	cout << "Le jeu va commencer!" << endl;
+	return 0;
+}

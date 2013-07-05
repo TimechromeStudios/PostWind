fd6d704423fe4fddda6a44345e2d792cc331c450
lacaulac <d0lph1n@me.com>
2013-07-05 23:48:01 +0200

    Some changes
    + Entity class
    - Bogged Gestion class, we'll use a simple int for IDs

---
+++ srclib/Entity.cpp
@@ -0,0 +1,29 @@
+Entity::Entity(int p_id, int p_health, string p_name)
+{
+	id = p_id;
+	health = p_health;
+	name = p_name;
+}
+
+int Entity::getId()
+{
+	return id;
+}
+
+int getHealth()
+{
+	return health;
+}
+
+std::string getName()
+{
+	return name;
+}
+void setName(std::string p_name)
+{
+	name = p_name;
+}
+void setHealth(int p_health)
+{
+	health = p_health;
+}
---
+++ srclib/Entity.h
@@ -0,0 +1,14 @@
+class Entity
+{
+private:
+	int id;
+	std::string name;
+	int health
+public:
+	Entity(int p_id, int p_health, string p_name);
+	int getId();
+	std::string getName();
+	void setName(std::string p_name);
+	int getHealth();
+	void setHealth(int p_health);
+};

d6498db905ef8cd595c8388425eceea18da6c7df
lacaulac <d0lph1n@me.com>
2013-07-05 22:34:40 +0200

    Name of the name updated
    We changed the default name to the name of the project

--- srclib/main.cpp
+++ srclib/main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <cstdlib>
+#include "Gestion.h"
 
 using namespace std;
 
 string version = "Pre-Alpha";
+Gestion central();
+int nombre;
 int main()
 {
-	cout << "Bienvenue dans la version " << version << " de ConsoleRPG!" << endl;
+	cout << "Bienvenue dans la version " << version << " de PostWind!" << endl;
 	cout << "Le jeu va commencer!" << endl;
 	return 0;
 }

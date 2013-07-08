fa389f4172c053b568966dc3a9349bb30132d3b6
lacaulac <d0lph1n@me.com>
2013-07-08 14:59:36 +0200

    Something

--- srclib/main.cpp
+++ srclib/main.cpp
@@ -4,15 +4,39 @@
 #include "Item.h" //Inclusion de la classe Item
 #include "Alive.h" //Inclusion de la classe Alive
 #include "Arme.h" //Inclusion de la classe Arme
+#include "libdlc.h" //Inclusion du ModSystem
+#include <vector> //Inclusion de la librairie vector
+#include <fstream> //Inclusion de la librairie fstream
 
 using namespace std; //Utilisation du namespace standard
 
-string version = "Pre-Alpha"; //Version du programme, affichée plus bas
+string version = "Pre-Alpha + ModSystem"; //Version du programme, affichée plus bas
 int entityId = 0; //Index d'entité, à incrémenter au fur et à mesure des créations d'entités
 
+<<<<<<< Updated upstream
 int main() //Fonction principale
 {
 	cout << "Bienvenue dans la version " << version << " de PostWind!" << endl; //Message de bienvenue
 	cout << "Le jeu va commencer!" << endl; //Là, je pense que vous avez compris
 	return 0; //Fin du programme
+=======
+int main() //Fonction principal
+	cout << "Bienvenue dans la version " << version << " de PostWind!" << endl; //Message de bienvenue
+	cout << "Le jeu va commencer!" << endl; //Là, je pense que vous avez compris
+	/*ifstream addonsList("addonsList.cfg");
+	DLCLibrary *addon;
+	if(addonsList)
+	{
+		string nomFichier;
+		function_type theFunction;
+		while(getline(addonsList, nomFichier))
+		{
+			addon = new DLCLibrary(nomFichier);
+			theFunction = *addon.getFunction<function_type>("start");
+			theFunction();
+			delete *addon;
+		}
+	}*/
+	return 0;
+>>>>>>> Stashed changes
 }

759014020c62a2287b1b447d93878775366ac624
lacaulac <d0lph1n@me.com>
2013-08-29 19:26:41 +0200

    XCode project added !
    + XCode project
    * Corriged compilation error with enum

---
+++ PostWind/PostWind/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  PostWind
+//
+//  Created by Antonin on 29/08/13.
+//  Copyright (c) 2013 Timechrome Studios. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
